d019f46f2b9fa0e3d4270b21feef96cdf7880607
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-12-02 20:00:57 +0100

    finished schema

--- schema.js
+++ schema.js
@@ -1,8 +1,42 @@
-import { 
-  GraphQLSchema, 
+import {
+  GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLList
 } from 'graphql'
+import fetch from 'node-fetch'
+
+const BASE_URL = 'http://localhost:3000'
+
+function getPersonByURL (relativeURL) {
+  fetch(`${BASE_URL}/${relativeURL}`)
+    .then(res => res.json())
+    .then(res => res.person)
+}
+const PersonType = new GraphQLObjectType({
+  name: 'Person',
+  decription: '...',
+
+  field: () => ({
+    firstName: {
+      type: GraphQLString,
+      resolve: (person) => person.first_name
+    },
+    lastName: {
+      type: GraphQLString,
+      resolve: (person) => person.last_name
+    },
+    email: { type: GraphQLString },
+    username: { type: GraphQLString },
+    id: { type: GraphQLString },
+    friends: {
+      type: GraphQLList(PersonType),
+      resolve: (person) =>
+        person.friends.map(getPersonByURL)
+    }
+
+  })
+})
 
 const QueryType = new GraphQLObjectType({
   name: 'Query',
@@ -13,7 +47,8 @@ const QueryType = new GraphQLObjectType({
       args: {
         id: { type: GraphQLString }
       },
-      resolve: () => 
+      resolve: (root, args) =>
+        getPersonByURL(`people/${args.id}`)
     }
   })
 })
